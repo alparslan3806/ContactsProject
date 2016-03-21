0dd8eed094ec3a06d67c6759c2e932e8a9264853
alparslan3806 - Alpaslan Develioğlu <alparslan3806@gmail.com>
2016-03-21 16:14:29 +0200

    My project is now capable of searching. It looks to phone numbers in my Contacts list. And according to radiobutton, which is clicked, it shows the phone number according to their operator.

--- app/src/main/java/alp/contactsproject/MainActivity.java
+++ app/src/main/java/alp/contactsproject/MainActivity.java
@@ -1,112 +1,129 @@
 package alp.contactsproject;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.widget.ListAdapter;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.SimpleAdapter;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends Activity {
 
     private ListView listView;
-    private TreeMap<String, String> phoneAndNameMap = new TreeMap<>();
+    private RadioButton aveaRadioButton,turkcellRadioButton,vodafoneRadioButton;
+    private List<Map<String, String>> phoneAndNameMap = new ArrayList<>();
+    private Map<String, String> item = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.list);
 
         Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-        String[] projection = new String[] {
+        String[] projection = new String[]{
                 ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-                ContactsContract.CommonDataKinds.Phone.NUMBER };
+                ContactsContract.CommonDataKinds.Phone.NUMBER};
         Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
         cursor.moveToFirst();
         int indexName = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
 
         int indexNumber = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
 
-        do{
+
+        item = new HashMap();
+        do {
+            
+
             String name = cursor.getString(indexName);
             String number = cursor.getString(indexNumber);
 
-            phoneAndNameMap.put(number, name);
+            item.put(name, number);
+            phoneAndNameMap.add(item);
 
-        }while(cursor.moveToNext());
-
-        getAvea(phoneAndNameMap);
-
-        //setListAdapter( phoneAndNameMap);
-        listView = (ListView) findViewById(android.R.id.list);
-        listView = getListView();
-        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        } while (cursor.moveToNext());
 
 
+        aveaRadioButton = (RadioButton) findViewById(R.id.aveaRadioButton);
+        turkcellRadioButton = (RadioButton) findViewById(R.id.turkcellRadioButton);
+        vodafoneRadioButton = (RadioButton) findViewById(R.id.vodafoneRadioButton);
 
     }
 
-    public TreeMap<String, String> getAvea(TreeMap<String, String> numberAndNameMap)
+    public void printHasMap(List<Map<String, String>> listMap)
     {
-        TreeMap<String, String> aveaMap = new TreeMap<>();
-        Iterator<Map.Entry<String, String>> map = numberAndNameMap.entrySet().iterator();
-        while(map.hasNext())
-        {
-            Map.Entry<String, String> entry = map.next();
-            if(entry.getKey().charAt(0) == '+' && entry.getKey().charAt(7) == '5')
-            {
-                aveaMap.put(entry.getKey(), entry.getValue());
+        SimpleAdapter adapter = new SimpleAdapter(this, listMap, R.layout.item,
+                new String[] {"userIcon", "userName", "userNumber" }, new int[] {R.id.userIcon, R.id.username, R.id.usertext});
 
-            }else if(entry.getKey().charAt(3) == '5'){
-                aveaMap.put(entry.getKey(), entry.getValue());
+        listView.setAdapter(adapter);
+
+        /**
+         * Iterator<Map.Entry<String, String>> entries = map.entrySet().iterator();
+         * String printNumber = null, printName = null;
+         * while (entries.hasNext()) {
+         * Map.Entry<String, String> entry = entries.next();
+         * printNumber += entry.getKey();
+         * printName += entry.getValue();
+         */
+    }
+
+    public void radioButtonOnClick(View view)
+    {
+        int id = view.getId();
+        List<Map<String, String>> temp = null;
+
+        switch (id)
+        {
+            case R.id.aveaRadioButton:
+                temp = getOperator(item, '5');
+                printHasMap(temp);
+                aveaRadioButton.setChecked(true);
+                break;
+            case R.id.turkcellRadioButton:
+                temp = getOperator(item, '3');
+                printHasMap(temp);
+                turkcellRadioButton.setChecked(true);
+                break;
+            case R.id.vodafoneRadioButton:
+                temp = getOperator(item, '4');
+                printHasMap(temp);
+                vodafoneRadioButton.setChecked(true);
+        }
+    }
+
+
+    public List<Map<String, String>> getOperator(Map<String, String> numberAndNameMap, char number1) {
+        List<Map<String, String>> listOperator = new ArrayList<>();
+
+
+        Iterator<Map.Entry<String, String>> map = numberAndNameMap.entrySet().iterator();
+        if(map != null) {
+            while (map.hasNext()) {
+                Map.Entry<String, String> entry = map.next();
+                Map operatorMap = new HashMap();
+                if ((entry.getValue().charAt(0) == '+') && (entry.getValue().charAt(7) == number1)) {
+                    operatorMap.put("userIcon", R.drawable.nouser);
+                    operatorMap.put("userName", entry.getKey());
+                    operatorMap.put("userNumber", entry.getValue());
+                    listOperator.add(operatorMap);
+
+                } else if (entry.getValue().charAt(3) == number1) {
+                    operatorMap.put("userIcon", R.drawable.nouser);
+                    operatorMap.put("userName", entry.getKey());
+                    operatorMap.put("userNumber", entry.getValue());
+                    listOperator.add(operatorMap);
+                }
             }
         }
-        return aveaMap;
+        return listOperator;
     }
-
-    public TreeMap<String, String> getTurkcell(TreeMap<String, String> numberAndNameMap)
-    {
-        TreeMap<String, String> turkcellMap = new TreeMap<>();
-        Iterator<Map.Entry<String, String>> map = numberAndNameMap.entrySet().iterator();
-        while(map.hasNext())
-        {
-            Map.Entry<String, String> entry = map.next();
-            if(entry.getKey().charAt(0) == '+' && entry.getKey().charAt(7) == '3')
-            {
-                turkcellMap.put(entry.getKey(), entry.getValue());
-            }else if(entry.getKey().charAt(3) == '3'){
-                turkcellMap.put(entry.getKey(), entry.getValue());
-            }
-        }
-        return turkcellMap;
-    }
-
-    public TreeMap<String, String> getVodafone(TreeMap<String, String> numberAndNameMap)
-    {
-        TreeMap<String, String> vodafoneMap = new TreeMap<>();
-        Iterator<Map.Entry<String, String>> map = numberAndNameMap.entrySet().iterator();
-        while(map.hasNext())
-        {
-            Map.Entry<String, String> entry = map.next();
-            if(entry.getKey().charAt(0) == '+' && entry.getKey().charAt(7) == '4')
-            {
-                vodafoneMap.put(entry.getKey(), entry.getValue());
-            }else if(entry.getKey().charAt(3) == '4'){
-                vodafoneMap.put(entry.getKey(), entry.getValue());
-            }
-        }
-        return vodafoneMap;
-    }
-
-
 }
